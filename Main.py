ba475fffc6fc3d45f8b196e758da6de08e64a502
Heung Choi <hung_guk@mail.com>
2020-11-19 23:07:12 -0700

    fixed code to show the background image, added seagull as a player, Choi 11.19.2020

--- Main.py
+++ Main.py
@@ -14,10 +14,8 @@ class Menu_view(arcade.View):
     def on_draw(self):
         directory = os.path.dirname(__file__) #Bro Manley
         filepath = directory + '/pictures/background_image.jpg'#Bro Manley
-        #folder = os.path.dirname(os.path.abspath(__file__)) + "\pictures\\"
         arcade.start_render()
-        self.background = arcade.load_texture(filepath)
-        arcade.draw_lrwh_rectangle_textured(0, 0, WIDTH, HEIGHT, self.background)
+        arcade.draw_lrwh_rectangle_textured(0, 0, WIDTH, HEIGHT, filepath)
         arcade.draw_text('Menu Screen', WIDTH/2, HEIGHT/2, arcade.color.WHITE, font_size = 50, anchor_x='center')
         arcade.draw_text('Click to Advance', WIDTH/2, HEIGHT/2 - 75, arcade.color.WHITE, font_size=50, anchor_x='center')
     
@@ -30,17 +28,14 @@ class Menu_view(arcade.View):
 class MyGame(arcade.Window):
     def __init__(self):
         super().__init__(WIDTH, HEIGHT, TITLE)
-        arcade.set_background_color(arcade.color.WHITE)
-        directory = os.path.dirname(__file__) #Bro Manley
-        filepath = directory + '/pictures/red_square.jpg'
-        #filepath = os.path.join(directory, "/pictures", 'red_square.jpg') #Bro Manley
-        #folder = os.path.dirname(os.path.abspath(__file__)) + "\pictures\\"
-        self.player_sprite = arcade.Sprite(filepath, 0.5) #Bro Manley
+        directory = os.path.dirname(__file__) # Load all directory path for image files.Choi
+        self.background = None #variable used in draw function to show background image.Choi
+        filepath_seagull = directory + '/pictures/seagull.png'
+        self.player_sprite = arcade.Sprite(filepath_seagull, 2) #variable to show the player with a scale.Choi
         self.physics_engine = None
-        self.total_time = 0.0 # to add time
+        self.total_time = 0.0 # to add time.Choi
 
     def setup(self):
-        arcade.set_background_color(arcade.color.WHITE)
         self.player_list = arcade.SpriteList()
         self.wall_list = arcade.SpriteList(use_spatial_hash=True)
         #Spatial hashing speeds the time it takes to find collisions, but increases the time it takes to move a sprite.
@@ -48,34 +43,29 @@ class MyGame(arcade.Window):
         self.player_sprite.center_x = 100
         self.player_sprite.center_y = 100
         self.player_list.append(self.player_sprite)
-        directory = os.path.dirname(__file__) #Bro Manley
-        filepath = directory + '/pictures/wall.jpg' #Bro Manley
-        #folder = os.path.dirname(os.path.abspath(__file__)) + "\pictures\\"
+        directory = os.path.dirname(__file__) # Load all directory path for image files.Choi
+        filepath_mcd = directory + '/pictures/mcdonalds.jpg'
+        filepath_wall = directory + '/pictures/wall.jpg'
+        self.background = arcade.load_texture(filepath_mcd)
         for x in range(0, 1500, 32):
-            wall = arcade.Sprite(filepath)
+            wall = arcade.Sprite(filepath_wall)
             wall.center_x = x
             wall.center_y = 50
             self.wall_list.append(wall)
         
         self.physics_engine = arcade.PhysicsEnginePlatformer(self.player_sprite, self.wall_list, GRAVITY)
-        self.total_time = 0.0 # to add time
+        self.total_time = 0.0 # to add time.Choi
 
     def on_draw(self):
         arcade.start_render()
-        self.wall_list.draw()
-        self.player_list.draw()
-
-        # Calculate minutes
-        minutes = int(self.total_time) // 60
-
-        # Calculate seconds by using a modulus (remainder)
-        seconds = int(self.total_time) % 60
-
-        # Figure out time output
-        output = (f"Time: {minutes:02d}:{seconds:02d}")
-
-        # Output the timer text.
-        arcade.draw_text(output, 300, 300, arcade.color.RED, 30)
+        arcade.draw_lrwh_rectangle_textured(0, 0, WIDTH, HEIGHT, self.background) # display McDonald background first.Choi
+        self.wall_list.draw() # display bottom green floor on top of McDonald background.Choi
+        self.player_list.draw() # display red square(player).Choi
+        
+        minutes = int(self.total_time) // 60 # Calculate minutes.Choi   
+        seconds = int(self.total_time) % 60 # Calculate seconds by using a modulus (remainder).Choi
+        output = (f"{minutes:02d}:{seconds:02d}") # Display time output.Choi
+        arcade.draw_text(output, WIDTH/2, HEIGHT -50, arcade.color.RED, 30) # Display the timer text.Choi
 
     def on_key_press(self, key, modifiers):
         if key == arcade.key.UP or key == arcade.key.W:
@@ -100,13 +90,14 @@ class MyGame(arcade.Window):
 
     def on_update(self, delta_time):
         self.physics_engine.update()
+
         #colision check for player
-        if self.player_sprite.center_x  > 1500 - 100:
+        if self.player_sprite.center_x  > WIDTH - 30:
             self.player_sprite.center_x += -5
         elif self.player_sprite.center_x < 0 + 30:
             self.player_sprite.center_x += 5
         
-        self.total_time += delta_time
+        self.total_time += delta_time #Update time.Choi
         #arcade.check_for_collision_with_list()
         #arcade.check_for_collision()
         #We can use one of these to help with the collions between players
@@ -115,10 +106,9 @@ class MyGame(arcade.Window):
 if __name__ == "__main__":
     
     #view = arcade.Window(WIDTH, HEIGHT, TITLE)
-    menu_view = Menu_view()
+    #menu_view = Menu_view()
     #view.show_view(menu_view)
     
     mygame = MyGame()
-    magame.show_view(menu_view)
     mygame.setup()
     arcade.run()
